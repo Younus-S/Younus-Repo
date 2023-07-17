47cc5689224732f4b533504e943bd7ac1a77d864
Younus-S <sulimanyyounus@gmail.com>
2023-07-17 21:21:51 +0500

    Assignment 0.6 added

---
+++ Assignments/Assignment_0.6.py
@@ -0,0 +1,8 @@
+year = 365
+day = 24
+hour = 60
+minute = 60
+second = 1
+print("Number of seconds in a year:", second*minute*hour*day*year)
+print("Number of minutes in a year:", minute*hour*day*year)
+print("Number of hours in a year:", hour*day*year)
