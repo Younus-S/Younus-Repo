9d2894183f0c26523d3d1dd1c293946d7fa8820c
Younus-S <sulimanyyounus@gmail.com>
2023-07-22 00:15:06 +0500

    Assignment 0.8 Operators added

---
+++ Assignments/0.8_AssignmentOperators.py
@@ -0,0 +1,30 @@
+print(4+5)
+print(4-5)
+print(4*5)
+print(4/5)
+print(4**5)
+print(4//5)
+print(4%5)
+
+x=4
+x+=5
+print(x)
+x-=5
+print(x)
+x*=5
+print(x)
+x/=5
+print(x)
+x**=5
+print(x)
+x//=5
+print(x)
+
+print(4<5)
+print(4>5)
+print(4<=5)
+
+z=4
+y=x
+print(z is x)
+print(y is x)
